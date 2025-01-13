01a6e115f99189540ffef38f9231546d291e4817
Russell Gordon <rgordon@lcs.on.ca>
2025-01-12 19:00:17 -0500

    Initial Commit

---
+++ IceCreamFlavours/IceCreamFlavoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamFlavoursApp.swift
+//  IceCreamFlavours
+//
+//  Created by Russell Gordon on 2025-01-12.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamFlavoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
